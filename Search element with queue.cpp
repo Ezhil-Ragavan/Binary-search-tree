c047961265954dbfc99a118f40a7d06cfe0af88d
Ezhil Ragavan A <147179407+Ezhil-Ragavan@users.noreply.github.com>
2024-11-12 14:48:57 +0530

    Search element with queue.cpp

--- Search element with queue.cpp
+++ Search element with queue.cpp
@@ -12,24 +12,19 @@ bool search_element(Node* root,int data)
         {
             Node *present=q.front();
             q.pop();
+            if(present->key==data)
+            {
+                return true;
+            }
             if(present->left!=NULL)
             {
                 q.push(present->left);
-                if(present->left->key == data)
-                {
-                    return true;
-                }
             }
             if(present->right!=NULL)
             {
                 q.push(present->right);
-                if(present->right->key==data)
-                {
-                    return true;
-                }
             }
-            return false;
         }
     }
-    return root;
+    return false;
 }
